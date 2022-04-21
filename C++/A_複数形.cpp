69263c6adae44fdb862e3eeb92926bd59a51f508
hisanori-ito <86972518+hisanori-ito@users.noreply.github.com>
2022-04-21 21:17:21 +0900

    training

---
+++ C++/A_複数形.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string W;
+  char c = 's';
+  
+  cin >> W;
+  
+  cout << W + c << endl;
+}
---
+++ C++/A_高橋くんの研修.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string evil, satan;
+  
+  cin >> evil >> satan;
+  
+  if (evil.size() > satan.size()) {
+    cout << evil << endl;
+  }
+  else {
+    cout << satan << endl;
+  }
+}
